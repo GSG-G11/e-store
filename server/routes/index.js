d06238991672cb6def5c7e698c0089dbc441ac82
yousra Jaradh <yousrakhaleel@gmail.com>
2022-04-03 14:34:24 +0300

    use handle error Relates#5

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
+const {notFoundPage, serverError} = require('../controllers');
 
+router.use(notFoundPage);
+router.use(serverError);
 module.exports = router;

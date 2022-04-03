a5ab781f31a517cd39d4aa615bec7daaef686e88
yousra Jaradh <yousrakhaleel@gmail.com>
2022-04-03 17:38:16 +0300

    add route delete Relates #9

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 const { clientError, serverError } = require('./error');
 const addProduct = require('./products/addProduct');
+const deleteProduct = require('./products/deleteProduct');
 
 module.exports = {
   clientError,
   serverError,
   addProduct,
+  deleteProduct
 };

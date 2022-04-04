c03e41e04115d77ba3ae49ca14aa4d98a4d4aafc
zaher-aa <zaherabuamro@gmail.com>
2022-04-04 12:29:37 +0300

    Added generic input handler and added routes for cart and homepage Relates #29

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
-import './index.css';
 import { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components';
+import './index.css';
 
 class App extends Component {
+  state = {
+    searchTerm: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSearch = ({ key }) => {
+    const { searchTerm } = this.state;
+
+    if (key === 'Enter' && searchTerm !== '') {
+      // Do Stuff...
+    }
+  };
+
   render() {
+    const { searchTerm } = this.state;
+
     return (
       <>
-        <h1>test</h1>
+        <Router>
+          <Navbar
+            searchTerm={searchTerm}
+            handleChange={this.handleChange}
+            handleSearch={this.handleSearch}
+          />
+          <Routes>
+            <Route path="/" element="home" />
+            <Route path="/cart" element="cart" />
+          </Routes>
+        </Router>
       </>
     );
   }
 }
+
 export default App;

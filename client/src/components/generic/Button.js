6836102e9df8f1071573604cfa1aab234008f907
zaher-aa <zaherabuamro@gmail.com>
2022-04-04 13:02:40 +0300

    Edited Icon component to use a custom parent className Relates #29

--- client/src/components/generic/Button.js
+++ client/src/components/generic/Button.js
@@ -1,19 +1,12 @@
 import './Button.css';
-const Button =(props)=>{
-  const {
-    children,
-    onClick,
-    ...rest
-  } = props;
+const Button = (props) => {
+  const { children, onClick, ...rest } = props;
+
   return (
-    <button
-      className='button'
-      onClick={onClick}
-      {...rest}
-    >
+    <button className="button" onClick={onClick} {...rest}>
       {children}
     </button>
   );
-}
+};
 
 export default Button;

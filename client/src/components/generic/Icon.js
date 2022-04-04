e072a323f2f7137162a4c9413713d86c2a0e6d4e
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-04 12:02:26 +0300

    updating icon component Relates #39

--- client/src/components/generic/Icon.js
+++ client/src/components/generic/Icon.js
@@ -1,22 +1,12 @@
-import './Icon.css'
-const Icon=(props)=>{
-  const {
-    type,
-    children,
-    onClick,
-    ...rest
-  } = props;
+import './Icon.css';
+const Icon = (props) => {
+  const { type, children, onClick, ...rest } = props;
   return (
-      <div onClick={onClick} className='icon-container'>
-    <i
-      className={`fa fa-${type}`}
-      
-      {...rest}
-      id='icon'
-    >
-      {children}
-    </i>
+    <div onClick={onClick} className="icon-container">
+      <i className={`fa fa-${type}`} {...rest} id="icon">
+        {children}
+      </i>
     </div>
   );
-}
+};
 export default Icon;

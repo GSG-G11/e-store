4465b6f3f7110889915ddbe9cf0221503571aa8e
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-04 11:59:43 +0300

    add generic icon Relates #39

---
+++ client/src/components/generic/Icon.js
@@ -0,0 +1,22 @@
+import './Icon.css'
+const Icon=(props)=>{
+  const {
+    type,
+    children,
+    onClick,
+    ...rest
+  } = props;
+  return (
+      <div onClick={onClick} className='icon-container'>
+    <i
+      className={`fa fa-${type}`}
+      
+      {...rest}
+      id='icon'
+    >
+      {children}
+    </i>
+    </div>
+  );
+}
+export default Icon;

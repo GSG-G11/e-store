75be93e54d9c38593d60640c3cade6bbc965e80c
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-04-04 11:34:45 +0300

    add name for label

--- client/src/components/generic/Input.jsx
+++ client/src/components/generic/Input.jsx
@@ -4,6 +4,7 @@ const Input = ({ name ,type, value, handleChange}) => {
     return(
         <>
             <label>
+                {name}
                 <input name={name} type={type} value={value} onChange={handleChange}></input>
             </label>
         </>

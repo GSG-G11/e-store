a74d62196acd799adf4a409e9429ee379f6faf53
zaher-aa <zaherabuamro@gmail.com>
2022-04-05 17:59:55 +0300

    Added tests for routes Relates #23

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -30,3 +30,84 @@ describe('GET /not-found', () => {
       });
   });
 });
+
+describe('GET /products', () => {
+  it('should return 200 OK and Content-Type /json/', () => {
+    request(app)
+      .get('/api/v1/products')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        expect(res.body.message).toBe('Success');
+      });
+  });
+});
+
+describe('GET /product/:id', () => {
+  it('should return 200 OK and Content-Type /json/', () => {
+    request(app)
+      .get('/api/v1/product/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        expect(res.body.product).toEqual([{
+          category: 'laptop',
+          description: 'laptop omen beautiful and nice',
+          id: 1,
+          img: 'https://www.thoughtco.com/thmb/NRuMaaVBhsrz3AyDBweiPAZpYfw=/1500x844/smart/filters:no_upscale()/GettyImages-909076272-5c48c8c146e0fb0001891c02.jpg',
+          name: 'omen',
+          price: '1200',
+        }]);
+      });
+  });
+});
+
+describe('POST /product/', () => {
+  it('should return 201 Created and Content-Type /json/', async () => {
+    request(app)
+      .post('/api/v1/product')
+      .send({
+        category: 'laptop',
+        description: 'laptop omen beautiful and nice',
+        name: 'omen',
+        price: '1200',
+        img: 'https://fake.img',
+      })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        expect(res.body.message).toBe('Success');
+      });
+  });
+});
+
+describe('PUT /product/:id', () => {
+  it('should return 200 OK and Content-Type /json/', async () => {
+    request(app)
+      .put('/api/v1/product/1')
+      .send({
+        category: 'laptop',
+        description: 'laptop omen beautiful and nice',
+        name: 'omen',
+        price: '1200',
+        img: 'https://fake.img',
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        expect(res.body.message).toBe('Success');
+      });
+  });
+});
+
+describe('DELETE /product/:id', () => {
+  it('should return 200 OK and Content-Type /json/', () => {
+    request(app)
+      .delete('/api/v1/product/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        expect(res.body.message).toBe('Success delete product');
+      });
+  });
+});
--- server/controllers/error/serverError.js
+++ server/controllers/error/serverError.js
@@ -1,5 +1,4 @@
 module.exports = (err, req, res, next) => {
-  console.log(err);
   res
     .status(err.status || 500)
     .json({

45ca04269362fbac692865e538ccae98e8632947
zaher-aa <zaherabuamro@gmail.com>
2022-04-05 22:19:52 +0300

    edited product query to return the edited product Relates #66

--- server/controllers/products/editProduct.js
+++ server/controllers/products/editProduct.js
@@ -3,12 +3,16 @@ const { productsSchema, CustomErr } = require('../../utils');
 
 const editProduct = (req, res, next) => {
   const { id } = req.params;
-  productsSchema.validateAsync(req.body).then(() => editProductQuery(req.body, id)).then(() => res.json({ message: 'Success', status: 200 })).catch((err) => {
-    if (err.details) {
-      next(CustomErr(err.details[0].message, 400));
-    } else {
-      next(err);
-    }
-  });
+  productsSchema
+    .validateAsync(req.body)
+    .then(() => editProductQuery(req.body, id))
+    .then(({ rows }) => res.json({ message: 'Success', status: 200, product: rows[0] }))
+    .catch((err) => {
+      if (err.details) {
+        next(CustomErr(err.details[0].message, 400));
+      } else {
+        next(err);
+      }
+    });
 };
 module.exports = editProduct;
--- server/database/queries/products/editProduct.js
+++ server/database/queries/products/editProduct.js
@@ -3,7 +3,7 @@ const connections = require('../../config/connections');
 const editProductQuery = ({
   name, price, category, img, description,
 }, id) => connections.query({
-  text: 'UPDATE products SET name=$2, price=$3 ,category=$4 ,img=$5, description=$6 WHERE id=$1',
+  text: 'UPDATE products SET name=$2, price=$3 ,category=$4 ,img=$5, description=$6 WHERE id=$1 RETURNING *',
   values: [id, name, price, category, img, description],
 });
 module.exports = editProductQuery;

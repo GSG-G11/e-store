b2917a24e6528919df0b74d879483354cd57d31d
zaher-aa <zaherabuamro@gmail.com>
2022-04-05 14:17:02 +0300

    Added the added product to the response body Relates #53

--- server/controllers/products/addProduct.js
+++ server/controllers/products/addProduct.js
@@ -2,12 +2,16 @@ const { addProductQuery } = require('../../database/queries');
 const { productsSchema, CustomErr } = require('../../utils');
 
 const addProduct = (req, res, next) => {
-  productsSchema.validateAsync(req.body).then(() => addProductQuery(req.body)).then(() => res.json({ message: 'Success', status: 201 })).catch((err) => {
-    if (err.details) {
-      next(CustomErr(err.details[0].message, 400));
-    } else {
-      next(err);
-    }
-  });
+  productsSchema
+    .validateAsync(req.body)
+    .then(() => addProductQuery(req.body))
+    .then(({ rows }) => res.json({ message: 'Success', status: 201, product: rows[0] }))
+    .catch((err) => {
+      if (err.details) {
+        next(CustomErr(err.details[0].message, 400));
+      } else {
+        next(err);
+      }
+    });
 };
 module.exports = addProduct;
